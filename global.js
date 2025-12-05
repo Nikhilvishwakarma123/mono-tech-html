0f837ffe5fa43bc1891b8bb511d614289774780e
Nikhil Vishwakarma <119350342+Nikhilvishwakarma123@users.noreply.github.com>
2025-12-05 12:41:35 +0530

    Add Typed.js functionality for text animation

---
+++ global.js
@@ -0,0 +1,8 @@
+new Typed('#typed', {
+    strings:
+        ['Precision Engineering for Heavy-Duty Metalworking',
+            'Precision Engineering for Heavy-Duty Metalworking'],
+    typeSpeed: 50,
+    delaySpeed: 10,
+    loop: true,
+});
